29261e9e28b4d8d0ae2ba1b8947d67a954d8103c
colle_d <darkfeyt@gmail.com>
2014-03-29 19:09:16 +0800

    fixed 'double new game' bug on Android (keyboard_input_manager.js:144)

--- www/js/keyboard_input_manager.js
+++ www/js/keyboard_input_manager.js
@@ -140,5 +140,6 @@ KeyboardInputManager.prototype.keepPlaying = function (event) {
 KeyboardInputManager.prototype.bindButtonPress = function (selector, fn) {
   var button = document.querySelector(selector);
   button.addEventListener("click", fn.bind(this));
-  button.addEventListener(this.eventTouchend, fn.bind(this));
+  // cause "double new game" bug on Android
+  //button.addEventListener(this.eventTouchend, fn.bind(this));
 };
